9f34aa58a8013cbdb08bf0f635d0a45cfcdd3f75
reasure3 <reasure3@naver.com>
2022-01-31 18:55:58 +0900

    Initial commit

--- src/main/java/com/reasure/practice/PracticeMod.java
+++ src/main/java/com/reasure/practice/PracticeMod.java
@@ -1,6 +1,5 @@
 package com.reasure.practice;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +11,6 @@ public class PracticeMod {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public PracticeMod() {
-        MinecraftForge.EVENT_BUS.register(this);
+
     }
 }
